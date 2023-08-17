fd3594922fc977f770d27075f118aa9a711b83fc
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-08-17 22:12:10 +0300

    start3

--- src/index.js
+++ src/index.js
@@ -18,14 +18,14 @@ fetch("https://tasty-treats-backend.p.goit.global/api/recipes/popular")
 
         return `<li class="popular-recipe-list-li">
         <div class="favorite-list-img">
-        <img src="${data.preview}" alt="${data.title}">
+        <img class="round-img-1" src="${data.preview}" alt="${data.title}">
       </div>
         <div class="popular-recipe-div-list">
           <h2 class="favorite-list-h2">${data.title}</h2>
-          <h3 class="favorite-list-h3">${data.description}</h3>
+          <h3 class="favorite-list-h3" >${data.description}</h3>
         </div>
-      </li>
-        `;
+        </li>`;
     });
     recipe_list.insertAdjacentHTML('beforeend', recipe.join(''));
   });
+
