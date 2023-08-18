7ce70d4c0d4770a16ccd647aca42fd3fc331305a
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-08-18 10:34:18 +0300

    start5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-import axios from 'axios';
 const recipe_list = document.querySelector('.popular-recipe-list');
-
-const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes/popular';
-
 
 fetch("https://tasty-treats-backend.p.goit.global/api/recipes/popular")
   .then(response => {
@@ -10,12 +6,10 @@ fetch("https://tasty-treats-backend.p.goit.global/api/recipes/popular")
     if (!response.ok) {
       throw new Error(`Ошибка запроса: ${response.status}`);
     }
-
     return response.json();
   })
   .then(data => {
     const recipe = data.map((data) => {
-
         return `<li class="popular-recipe-list-li">
         <div class="favorite-list-img">
         <img class="round-img-1" src="${data.preview}" alt="${data.title}">
@@ -28,4 +22,3 @@ fetch("https://tasty-treats-backend.p.goit.global/api/recipes/popular")
     });
     recipe_list.insertAdjacentHTML('beforeend', recipe.join(''));
   });
-
